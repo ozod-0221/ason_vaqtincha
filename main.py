b0bbf63f3d4f35096ef4cfb3f065e3c1ecf30e34
Ozodbek Sobirjonov <118391973+ozod-0221@users.noreply.github.com>
2025-04-10 23:26:06 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ import logging
 from config import *
 from keyboards import *
 from helpers import *
+from json_utils import *
 from aiogram.types import ReplyKeyboardRemove
-
+admin_filter = F.from_user.id.in_(ADMIN_IDS)
 bot=Bot(token=TOKEN)
 dp=Dispatcher()
 
@@ -18,13 +19,25 @@ class Form(StatesGroup):
     
     phone = State()
     check = State()
+class SendMessage(StatesGroup):
+
+    waiting_for_message= State()
+    
     
 @dp.message(Command("start"))
 async def start(message: Message,state:FSMContext):
+    user_id = message.from_user.id
+    if user_id in ADMIN_IDS:
+        await message.answer("Admin panelga xush kelibsiz!",reply_markup=admin_key())
+        return
     name= message.from_user.first_name
     await message.answer(f"Assalomu alekum {name}, Xush kelibsiz!", reply_markup=contact_keyboard())
+    try:
+        save_user_id(message.from_user.id)
+    except Exception as e:
+        logging.error(f"Error: {e}")
     await state.set_state(Form.phone)
-    
+   
 @dp.message(Form.phone, F.contact)
 async def process_contact(message: Message, state: FSMContext,bot:Bot):
     message_id = message.message_id
@@ -52,15 +65,34 @@ async def check_subscription(user_id:int,bot:Bot) -> bool:
     except Exception as e:
         logging.error(f"Error checking subscription: {e}")
     return False 
+@dp.message(admin_filter,F.text == "💬 Xabar yuborish")
+async def send_message(message: Message,state: FSMContext):
+    await message.answer("Kanaldagi postning URL manzilini kiriting:")
+    await state.set_state(SendMessage.waiting_for_message)
+@dp.message(SendMessage.waiting_for_message)
+async def forward_message(message: Message, state: FSMContext,bot:Bot):
+    post_link = message.text
+    message_id = post_link.split("/")[-1]
+
+    # Kanaldagi postni olish
+    for user_id in get_all_user_ids() :
+        try:
+            await bot.forward_message(user_id,CHANNEL_ID,  message_id)
+            await asyncio.sleep(0.3)
+        except Exception as e:
+            logging.error(f"Xatolik sodir bo'ldi: {e}")
+    await message.answer("Xabar yuborildi!")
+    
+    await state.clear()
+    
+    
 @dp.callback_query(Form.check,F.data=="check")
 async def get_callback(callback:CallbackQuery,state:FSMContext,bot:Bot):
     user_id=callback.from_user.id
     
     
     user_data = await state.get_data()
-    print("📍📍📍📍📍📍📍📍📍📍")
-    print(user_data)
-    print("📍📍📍📍📍📍📍📍📍📍")
+   
     # Check if the user is subscribed to the channel
     if not await check_subscription(user_id,bot):
         await callback.answer("Iltimos, avval kanalga a'zo bo'ling", show_alert=True)
